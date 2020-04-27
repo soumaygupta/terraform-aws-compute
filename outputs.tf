2f3a1cea1d2c3ad57847ebeba8bfde6560fa0573
soumaygupta <soumay.gupta94@gmail.com>
2020-04-27 16:16:11 +0000

    Private IP Addition

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "ec2_instance_public_ips" {
   value       = module.ec2_instances.public_ip
 }
 
-output "ec2_instance_public_ips" {
-  description = "Public IP addresses of EC2 instances"
+output "ec2_instance_private_ips" {
+  description = "Private IP addresses of EC2 instances"
   value       = module.ec2_instances.private_ip
 }
