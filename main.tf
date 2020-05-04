0b5c0744b9ad6efd00ab650d83f1162a59a62446
soumaygupta <soumay.gupta94@gmail.com>
2020-05-04 11:42:35 +0000

    sample

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "ec2_instances" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 2.0"
-
+# sample
   name           = "sample_instance"
   instance_count = 2
   ami             = var.ami
